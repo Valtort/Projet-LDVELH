a880225d5aaec858d929136040d3af75e743af69
Valentin <valtort@hotmail.com>
2024-08-15 22:27:58 +0200

    Modification pour obtenir un meilleur rendu

--- Exo4.c
+++ Exo4.c
@@ -107,8 +107,12 @@ void free_livre(livre_t* livre){
 }
 
 
-int main(){
-    FILE* f = fopen("multiverse.txt","r");
+int main(int argc, char** argv){
+    if (argc != 2){
+      printf("Usage: %s [nom du fichier .txt]\n", argv[0]);
+      exit(1);
+    }
+    FILE* f = fopen(argv[1],"r");
     assert(f != NULL);
     livre_t* livre = generer_livre(f);
     bool jeu_en_cours = true;
